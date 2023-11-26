dd10bd52f619693e846748a97374f7ec3c600fc1
Dillon Awes <dillonawes@gmail.com>
2023-11-25 21:17:27 -0600

    conditional style and component rendering functionality implemented. Final touches on content.

--- src/index.js
+++ src/index.js
@@ -4,42 +4,42 @@ import "./index.css";
 
 const pizzaData = [
   {
-    name: "Focaccia",
-    ingredients: "Bread with italian olive oil and rosemary",
+    name: "Flatbrød",
+    ingredients: "Bread with Norwegian olive oil and rosemary",
     price: 6,
     photoName: "pizzas/focaccia.jpg",
     soldOut: false,
   },
   {
-    name: "Pizza Margherita",
+    name: "Tomat Og Mozzarella Pizza",
     ingredients: "Tomato and mozarella",
     price: 10,
     photoName: "pizzas/margherita.jpg",
     soldOut: false,
   },
   {
-    name: "Pizza Spinaci",
+    name: "Pizza Med Spinat",
     ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
     price: 12,
     photoName: "pizzas/spinaci.jpg",
     soldOut: false,
   },
   {
-    name: "Pizza Funghi",
+    name: "Soppizza",
     ingredients: "Tomato, mozarella, mushrooms, and onion",
     price: 12,
     photoName: "pizzas/funghi.jpg",
     soldOut: false,
   },
   {
-    name: "Pizza Salamino",
+    name: "Salamipizza",
     ingredients: "Tomato, mozarella, and pepperoni",
     price: 15,
     photoName: "pizzas/salamino.jpg",
     soldOut: true,
   },
   {
-    name: "Pizza Prosciutto",
+    name: "Prosciuttopizza",
     ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
     price: 18,
     photoName: "pizzas/prosciutto.jpg",
@@ -50,8 +50,6 @@ const pizzaData = [
 function App() {
   return (
     <div className="container">
-      <h1>Comfort Is a Slow Death.</h1>
-      <h1>Hello React!</h1>
       <Header />
       <Menu />
       <Footer />
@@ -71,17 +69,29 @@ function Header() {
 
 function Menu() {
   const pizzas = pizzaData;
+  // const pizzas = [];
   const numPizzas = pizzas.length;
   return (
     <main className="menu">
       <h2>Our Menu</h2>
 
-      {numPizzas > 0 && (
-        <ul className="pizzas">
-          {pizzaData.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+      {numPizzas > 0 ? (
+        <>
+          <p>
+            Norwegian Noshery: 7 whimsically Nordic delights to tickle your
+            taste buds! Cooked up with a dash of fjord flair and a sprinkle of
+            Viking spirit. Straight from our stone oven, all foraged from the
+            fjells, all lutefisk-free, all utterly scrumptious. Skål to
+            flavor-packed fun on a plate!
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>We're still working on our menu, please check back later!</p>
       )}
       {/* <Pizza
         name="Pizza Spinaci"
@@ -98,19 +108,20 @@ function Menu() {
     </main>
   );
 }
-function Pizza(props) {
-  console.log(props);
+// destructuring props { pizzaObj, canAddExtraPropHere, andYetAnotherHere }
+function Pizza({ pizzaObj }) {
+  console.log(pizzaObj);
+
+  // if (pizzaObj.soldOut) {
+  //   return <p>Sorry, the {pizzaObj.name} is currently sold out.</p>;
+  // }
   return (
-    <li className="pizza">
-      <img
-        className="pizza"
-        src={props.pizzaObj.photoName}
-        alt={props.pizzaObj.name}
-      />
-      <h3>{props.pizzaObj.name}</h3>
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+      <img className="pizza" src={pizzaObj.photoName} alt={pizzaObj.name} />
+      <h3>{pizzaObj.name}</h3>
       <div>
-        <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price}</span>
+        <p>{pizzaObj.ingredients}</p>
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
       </div>
     </li>
   );
@@ -132,16 +143,31 @@ function Footer() {
   // } else {
   //   alert("We are closed!");
   // }
+
   return (
     <div>
       <footer className="footer">
-        {isOpen && (
-          <div className="order">
-            <p>We're open until 11:00pm! Come visit us or oder online.</p>
-            <button className="btn">Order</button>
-          </div>
+        {isOpen ? (
+          <Order closeHour={closeHour} openHour={openHour} />
+        ) : (
+          <p>
+            Sorry, we're closed. Please come back and see us tomorrow at 10:00
+            am!
+          </p>
         )}
       </footer>
+    </div>
+  );
+}
+
+function Order({ closeHour, openHour }) {
+  return (
+    <div className="order">
+      <p>
+        We're open from {openHour}:00 until {closeHour}:00pm! Come visit us or
+        order online.
+      </p>
+      <button className="btn">Order</button>
     </div>
   );
 }
